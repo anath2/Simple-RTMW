9d603c4d93b0e7df018767ee8ad2148fb0643aa1
Ajit Nath <nath.ajit01@gmail.com>
2025-08-28 17:26:00 +0800

    Fixes class args and linting

--- simple_rtmw/base.py
+++ simple_rtmw/base.py
@@ -2,6 +2,7 @@ import os
 import logging
 from abc import ABCMeta, abstractmethod
 from typing import Any
+from pathlib import Path
 
 import numpy as np
 import onnxruntime as ort
@@ -12,48 +13,40 @@ logger = logging.getLogger(__name__)
 
 
 def check_mps_support():
-    try:
-        providers = ort.get_available_providers()
-        return 'MPSExecutionProvider' in providers or 'CoreMLExecutionProvider' in providers
-    except ImportError:
+    providers = ort.get_available_providers()
+    return "MPSExecutionProvider" in providers or "CoreMLExecutionProvider" in providers
+
+
+def model_exists(onnx_model: str) -> bool:
+    if not os.path.exists(onnx_model):
         return False
+    return True
 
 
-ONNX_SETTINGS = {
-    'onnxruntime': {
-        'cpu': 'CPUExecutionProvider',
-        'cuda': 'CUDAExecutionProvider',
-        'rocm': 'ROCMExecutionProvider',
-        'mps': 'CoreMLExecutionProvider' if check_mps_support() else 'CPUExecutionProvider'
-    },
+ONNX_DEVICES = {
+    "cpu": "CPUExecutionProvider",
+    "cuda": "CUDAExecutionProvider",
+    "rocm": "ROCMExecutionProvider",
+    "mps": "CoreMLExecutionProvider" if check_mps_support() else "CPUExecutionProvider",
 }
 
 
 class BaseTool(metaclass=ABCMeta):
-    def __init__(self,
-        onnx_model: str,
+    def __init__(
+        self,
+        model_url: str,
+        model_base_dir: Path,
         model_input_size: tuple,
-        mean: tuple,
-        std: tuple,
-        backend: str = 'onnxruntime',
-        device: str = 'cpu'
+        device: str = "cpu",
     ):
-        if not os.path.exists(onnx_model):
-            onnx_model = download_checkpoint(onnx_model)
-
-        providers = ONNX_SETTINGS[backend][device]
+        onnx_model = download_checkpoint(model_url, model_base_dir)
+        providers = ONNX_DEVICES[device]
         self.session = ort.InferenceSession(
-            path_or_bytes=onnx_model,
-            providers=[providers]
+            path_or_bytes=onnx_model, providers=[providers]
         )
-
-        logger.info(f'load {onnx_model} with {backend} backend')
-
+        logger.info(f"load {onnx_model} with {device} device")
         self.onnx_model = onnx_model
         self.model_input_size = model_input_size
-        self.mean = mean
-        self.std = std
-        self.backend = backend
         self.device = device
 
     @abstractmethod
@@ -61,7 +54,7 @@ class BaseTool(metaclass=ABCMeta):
         """Implement the actual function here."""
         raise NotImplementedError
 
-    def inference(self, img: np.ndarray) -> np.ndarray:
+    def inference(self, img: np.ndarray) -> Any:
         """Inference model.
 
         Args:
@@ -74,9 +67,10 @@ class BaseTool(metaclass=ABCMeta):
         img = img.transpose(2, 0, 1)
         img = np.ascontiguousarray(img, dtype=np.float32)
         input = img[None, :, :, :]
+
         sess_input = {self.session.get_inputs()[0].name: input}
         sess_output = []
-        
+
         for out in self.session.get_outputs():
             sess_output.append(out.name)
 
